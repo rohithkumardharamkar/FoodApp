a186de17d90d3f7068992bfb4f979d6db62fb6a8
Dharamkar Rohith Kumar <159691874+rohithkumardharamkar@users.noreply.github.com>
2024-12-22 15:10:40 +0530

    second
    second

---
+++ NamsteReact/dist/Grocery.c309ceb9.js
@@ -0,0 +1,2 @@
+var e=globalThis.parcelRequire667e;(0,e.register)("loR3J",function(r,a){Object.defineProperty(r.exports,"__esModule",{value:!0,configurable:!0}),Object.defineProperty(r.exports,"default",{get:()=>l,set:void 0,enumerable:!0,configurable:!0});var i=e("ayMG0"),l=function(){return/*#__PURE__*/(0,i.jsx)("div",{children:/*#__PURE__*/(0,i.jsx)("h1",{children:"Please wait for few Days we are launching Groceries page"})})}});
+//# sourceMappingURL=Grocery.c309ceb9.js.map
